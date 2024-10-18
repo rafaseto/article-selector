710c2696f9a619ca838fd86b328ddace43e3b42b
rafaseto <rafael08.0807@gmail.com>
2024-10-18 02:25:31 -0300

    feat: show next article after answer

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-    const [article, setArticle] = useState(null)
-    const [index, setIndex] = useState(0)
+    const [article, setArticle] = useState(null);
+    const [index, setIndex] = useState(0);
 
     useEffect(() => {
-        axios.get(`/article/${index}`)
+        axios.get(`http://localhost:5000/api/article/${index}`)
             .then(response => {
                 setArticle(response.data);
             })
             .catch(error => {
                 console.log("Article not found", error);
-            })
+            });
     }, [index]);
 
     const answer = (my_answer) => {
-        axios.post(`/answer`, {index: index, my_answer})
+        let action;
+
+        if (my_answer == 'accept') {
+            action = 'accept';
+        } else {
+            action = 'reject';
+        }
+
+        axios.post(`http://localhost:5000/api/answer`, {
+                index: index,
+                action: action    
+            })
             .then(response => {
-                alert(response.data.message);
                 setIndex(index + 1);
             })
             .catch(error => {
